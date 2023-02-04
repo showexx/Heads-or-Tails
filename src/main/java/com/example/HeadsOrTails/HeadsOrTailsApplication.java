909a07a2301f9da92fa13b9b85ddb6d3aae24cc5
showex <official.showex>
2023-02-04 23:00:43 +0300

    start

---
+++ src/main/java/com/example/HeadsOrTails/HeadsOrTailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.HeadsOrTails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadsOrTailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadsOrTailsApplication.class, args);
+	}
+
+}
