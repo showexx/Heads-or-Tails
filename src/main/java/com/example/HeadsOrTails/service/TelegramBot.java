49ecca3bc5bfd30ec3d7bd54eecbdee724a57c32
showex <official.showex>
2023-02-05 14:09:19 +0300

    connecting to telegram

---
+++ src/main/java/com/example/HeadsOrTails/service/TelegramBot.java
@@ -0,0 +1,53 @@
+package com.example.HeadsOrTails.service;
+
+import com.example.HeadsOrTails.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig botConfig;
+
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (messageText) {
+                case "/start":
+                    sendMessage(chatId, "Hello");
+                    break;
+            }
+        }
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try{
+            execute(message);
+        }
+        catch (TelegramApiException e) {
+
+        }
+    }
+}
